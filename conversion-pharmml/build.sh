e6f34fe1ced22816624e27df5ebf6b9a0dea2a70
Toni G <toni.giorgino@gmail.com>
2014-11-24 18:43:30 +0100

    first draft

--- conversion-pharmml/build.sh
+++ conversion-pharmml/build.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 # Notes for the conversion of Chen-Sherman's model in PharmML
-# ===========
 
-# The human-readable pieces are translated to PharmML via the 
-# http://infix2pharmml.sourceforge.net service.
 
-# Based directly off the original XPP version. There is some
-# bash-automation here.
+# The human-readable pieces are translated to PharmML via the
+# http://infix2pharmml.sourceforge.net service.  Based directly off
+# the original XPP version.
 
 c="perl ./convert.pl -q"
 
 
 # Heaviside function
-$c "heavx(x):= (abs(x)+x)/(2*abs(x)+1e-5)" > f_heavx.xml
+rm functions.xml
+$c "heavx(x):= (abs(x)+x)/(2*abs(x)+1e-5)" >> functions.xml
 
 
 # Modulus function
@@ -21,13 +20,12 @@ $c "heavx(x):= (abs(x)+x)/(2*abs(x)+1e-5)" > f_heavx.xml
 #PharmML has the `rem()` function. In case it does not accept
 #real-valued arguments, the following can be used.
 
-$c "modulus(a,b):=a-b*floor(a/b)" > f_modulus.xml
+$c "modulus(a,b):=a-b*floor(a/b)" >> functions.xml
 
 
 
 # Saturation function
-
-$c "minf(v):= 1/(1 + exp((Vm-V)/sm))" > f_minf.xml
+$c "minf(v):= 1/(1 + exp((Vm-V)/sm))" >> functions.xml
 
 
 
@@ -39,11 +37,25 @@ $c "minf(v):= 1/(1 + exp((Vm-V)/sm))" > f_minf.xml
 # Microdomain and cytosol Ca++ concentrations
 # Chain of granules
 
-$c -s "
+$c -q -s "
+IL:=gL*minf(v=V)*(V- Vca)
+IR:=0.25*IL
+
+JL:=alpha*IL/vmd
+JR:=alpha*IR/vcell
+
+Jserca := Jsercamax*Ci^2/(Kserca^2 + Ci^2)
+Jpmca := Jpmcamax*Ci/(Kpmca + Ci)
+Jncx := Jncx0*(Ci - 0.25)
+L:=Jserca + Jpmca + Jncx + Jleak
+
 V:=Vrest + (Vburst - Vrest)*(heav(x=rem(t, tcycle)) - heav(x=rem(t,tcycle) - toff))
 
 diff(Cmd,t):= ts*(-fmd*JL - fmd*B*(Cmd - Ci))
 diff(Ci,t):= ts*(-fi*JR + fv*fi*B*(Cmd - Ci) - fi*L)
+
+r2:=r20*Ci/(Ci + Kp2)
+r3:=GlucFact*r30*Ci/(Ci + Kp)
 
 diff(N1,t):= ts*(-(3*k1*Cmd + rm1)*N1 + km1*N2 + r1*N5)
 diff(N2,t):= ts*(3*k1*Cmd*N1 -(2*k1*Cmd + km1)*N2 + 2*km1*N3)
@@ -55,7 +67,20 @@ diff(NF,t):= ts*(u1*N4 - u2*NF)
 diff(NR,t):= ts*(u2*NF - u3*NR)
 
 diff(SE,t):= ts*(u3*NR)
-" > model.xml
+
+docked := N6 + N5 + N1
+primed := N5 + N1
+" > structural.xml
+
+# PharmML lacks the delay() function
+# measured:=4.5*(SE - delay(SE, tau))
 
 
+
+# Merge the two
+sed '/<!-- Insert FunctionDefinition here -->/ r functions.xml' structural.xml > model.xml
+
+
+# Final pp
+xml_pp -i model.xml
 
