07feeb45791b0804379ad2b4caed05f992743862
Toni G <toni.giorgino@gmail.com>
2014-11-24 18:17:05 +0100

    shellified

--- conversion-pharmml/build.sh
+++ conversion-pharmml/build.sh
@@ -13,7 +13,7 @@ c="perl ./convert.pl -q"
 
 
 # Heaviside function
-$c "heavx(x):= (abs(x)+x)/(2*abs(x)+1e-5)" > heavx.xml
+$c "heavx(x):= (abs(x)+x)/(2*abs(x)+1e-5)" > f_heavx.xml
 
 
 # Modulus function
@@ -21,13 +21,13 @@ $c "heavx(x):= (abs(x)+x)/(2*abs(x)+1e-5)" > heavx.xml
 #PharmML has the `rem()` function. In case it does not accept
 #real-valued arguments, the following can be used.
 
-$c "modulus(a,b):=a-b*floor(a/b)" > modulus.xml
+$c "modulus(a,b):=a-b*floor(a/b)" > f_modulus.xml
 
 
 
 # Saturation function
 
-$c "minf(v) = 1/(1 + exp((Vm-V)/sm))" > minf.xml
+$c "minf(v):= 1/(1 + exp((Vm-V)/sm))" > f_minf.xml
 
 
 
@@ -37,25 +37,27 @@ $c "minf(v) = 1/(1 + exp((Vm-V)/sm))" > minf.xml
 
 # Voltage source - a square source
 
-$c "V:=Vrest + (Vburst - Vrest)*(heav(x=rem(t, tcycle)) - heav(x=rem(t,tcycle) - toff))" > V.xml
+$c "V:=Vrest + (Vburst - Vrest)*(heav(x=rem(t, tcycle)) - heav(x=rem(t,tcycle) - toff))" > e_V.xml
 
 # Microdomain and cytosol Ca++ concentrations
 
-$c "diff(Cmd,t):= ts*(-fmd*JL - fmd*B*(Cmd - Ci))" > Cmd.xml
-$c "diff(Ci,t):= ts*(-fi*JR + fv*fi*B*(Cmd - Ci) - fi*L)" > Ci.xml
+$c "diff(Cmd,t):= ts*(-fmd*JL - fmd*B*(Cmd - Ci))" > ee_Cmd.xml
+$c "diff(Ci,t):= ts*(-fi*JR + fv*fi*B*(Cmd - Ci) - fi*L)" > ee_Ci.xml
 
 # Chain of granules
 
 (
-$c -q "diff(N1,t):= ts*(-(3*k1*Cmd + rm1)*N1 + km1*N2 + r1*N5)"
-$c -q "diff(N2,t):= ts*(3*k1*Cmd*N1 -(2*k1*Cmd + km1)*N2 + 2*km1*N3)"
-$c -q "diff(N3,t):= ts*(2*k1*Cmd*N2 -(2*km1 + k1*Cmd)*N3 + 3*km1*N4)"
-$c -q "diff(N4,t):= ts*(k1*Cmd*N3 - (3*km1 + u1)*N4)"
-$c -q "diff(N5,t):= ts*(rm1*N1 - (r1 + rm2)*N5 + r2*N6)"
-$c -q "diff(N6,t):= ts*(r3 + rm2*N5 - (rm3 + r2)*N6)"
-$c -q "diff(NF,t):= ts*(u1*N4 - u2*NF)"
-$c -q "diff(NR,t):= ts*(u2*NF - u3*NR)"
-) > chain.xml
+$c  "diff(N1,t):= ts*(-(3*k1*Cmd + rm1)*N1 + km1*N2 + r1*N5)"
+$c  "diff(N2,t):= ts*(3*k1*Cmd*N1 -(2*k1*Cmd + km1)*N2 + 2*km1*N3)"
+$c  "diff(N3,t):= ts*(2*k1*Cmd*N2 -(2*km1 + k1*Cmd)*N3 + 3*km1*N4)"
+$c  "diff(N4,t):= ts*(k1*Cmd*N3 - (3*km1 + u1)*N4)"
+$c  "diff(N5,t):= ts*(rm1*N1 - (r1 + rm2)*N5 + r2*N6)"
+$c  "diff(N6,t):= ts*(r3 + rm2*N5 - (rm3 + r2)*N6)"
+$c  "diff(NF,t):= ts*(u1*N4 - u2*NF)"
+$c  "diff(NR,t):= ts*(u2*NF - u3*NR)"
+
+$c "diff(SE,t):= ts*(u3*NR)"
+) > ee_chain.xml
 
 
 
