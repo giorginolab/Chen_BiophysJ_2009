0798385cb623a13e6980bc2cd10afa5c53fd517d
Toni <toni.giorgino@gmail.com>
2019-09-12 14:01:43 +0200

    submodule sf

--- conversion-pharmml/make.sh
+++ conversion-pharmml/make.sh
@@ -4,7 +4,7 @@
 # directly off the original XPP version.
 
 # Location of the http://infix2pharmml.sourceforge.net driver program.
-bash ./build.sh structural.infix2pharmml functions.infix2pharmml > model.xml
+bash infix2pharmml-code/build.sh structural.infix2pharmml functions.infix2pharmml > model.xml
 
 # Edit name and description
 sed -i -e 's|Untitled model|Chen_2009_BiophysJ_Exocytosis|' \
